b50d0af46fdb9540f12ce9110d898f230849210a
nsvk13 <nsvkdev@gmail.com>
2025-05-07 02:15:13 +0300

    feat/WIP: Initial extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { runYED } from './commandRunner';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+async function handle(mode: 'encrypt' | 'decrypt') {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {return;}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yed-extension" is now active!');
+  const selection = editor.document.getText(editor.selection);
+  if (!selection) {return;}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('yed-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from yed-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+  try {
+    const result = await runYED(mode, selection);
+    await editor.edit(edit => {
+      edit.replace(editor.selection, result);
+    });
+  } catch (err: any) {
+    vscode.window.showErrorMessage(err.message || String(err));
+  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('yed.encrypt', () => handle('encrypt')),
+    vscode.commands.registerCommand('yed.decrypt', () => handle('decrypt'))
+  );
+}
